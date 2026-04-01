2df7db6eaaeff5d0e850e024acac44eaf45fdde0
YAQIN SHAWKAN <s202255400@kfupm.edu.sa>
2026-04-01 13:34:12 +0300

    TODO 4 is done

--- utils/operation.js
+++ utils/operation.js
@@ -1,16 +1,20 @@
+import { result } from "lodash";
 
 export function add(numbers) {
-    return {}
+    return numbers.reduce((sum, num) => sum + num, 0);
     }
 
 export function subtract(numbers) {
-      return {}
+      return numbers.slice(1).reduce((result, num) => result - num, numbers[0]);
     }
 
 export function multiply(numbers) {
-      return {}
+      return numbers.reduce((product, num) => product * num, 1);
     }
 
 export function divide(numbers) {
-      return {}
+    if (numbers.slice(1).some(num => num === 0)) {
+        return "Error: Division by zero is not allowed.";
     }
+    return numbers.slice(1).reduce((res, num) => res / num, numbers[0]);
+}
